d28371aa93d83781791b5c2af51479b28070db5c
tapanbk <bktapan16@gmail.com>
2023-09-27 12:06:19 +0545

    [update] github workflow file

--- socket_channels/settings.py
+++ socket_channels/settings.py
@@ -92,6 +92,10 @@ DATABASES = {
     }
 }
 
+TEST = {
+    'NAME': 'test_postgres_database',
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
